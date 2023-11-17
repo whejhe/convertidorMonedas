ca5031ce7b41e3ec84642e6d9d64c9f6ec49cc38
Carlos <dominguezalacid@gmail.com>
2023-11-17 09:48:56 +0100

    actualizar

--- src/convert.js
+++ src/convert.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import  monedasLista  from './monedas.js';
+import monedasLista from './monedas.js';
 import './convert.css';
 
 const Formulario = () => {
@@ -10,43 +10,43 @@ const Formulario = () => {
     const handleEurosChange = (e) => {
         const valorEuros = parseFloat(e.target.value);
         setEuros(valorEuros);
-        const nuevoCambio = parseFloat(valorEuros * monedasLista.find(moneda => moneda.code === monedaSeleccionada).value.toFixed(2));
+        const nuevoCambio = parseFloat(valorEuros * monedasLista.find(moneda => moneda.code === monedaSeleccionada).value).toFixed(2);
         setCambio(nuevoCambio);
     };
 
     const handleCambioChange = (e) => {
         const valorCambio = parseFloat(e.target.value);
         setCambio(valorCambio);
-        const nuevoEuros = parseFloat(valorCambio / monedasLista.find(moneda => moneda.code === monedaSeleccionada).value.toFixed(2));
+        const nuevoEuros = parseFloat(valorCambio / monedasLista.find(moneda => moneda.code === monedaSeleccionada).value).toFixed(2);
         setEuros(nuevoEuros);
     };
 
     const handleMonedaChange = (e) => {
         const nuevaMoneda = e.target.value;
         setMonedaSeleccionada(nuevaMoneda);
-        const nuevaTasaDeCambio = parseFloat(euros * monedasLista.find(moneda => moneda.code === nuevaMoneda).value.toFixed(2));
+        const nuevaTasaDeCambio = parseFloat(euros * monedasLista.find(moneda => moneda.code === nuevaMoneda).value).toFixed(2);
         setCambio(nuevaTasaDeCambio);
     };
 
     return (
         <div className='title'>
             <h1>Convertidor de Moneda</h1>
-        <div className='form'>
-        <label>Selecciona moneda:</label>
-        <select value={monedaSeleccionada} onChange={handleMonedaChange}>
-                {monedasLista.map((moneda) => (
-                    <option key={moneda.code} value={moneda.code}>
-                        {moneda.code}
-                    </option>
-                ))}
-            </select>
-            <br></br>
-            <label>Euros:</label>
-            <input type="number" value={euros} onChange={handleEurosChange} />
+            <div className='form'>
+                <label>Selecciona moneda:</label>
+                <select value={monedaSeleccionada} onChange={handleMonedaChange}>
+                    {monedasLista.map((moneda) => (
+                        <option key={moneda.code} value={moneda.code}>
+                            {moneda.code}
+                        </option>
+                    ))}
+                </select>
+                <br></br>
+                <label>Euros:</label>
+                <input type="number" value={euros} onChange={handleEurosChange} />
 
-            <label>Cambio a {monedaSeleccionada}:</label>
-            <input type="number" value={cambio} onChange={handleCambioChange} />
-        </div>
+                <label>Cambio a {monedaSeleccionada}:</label>
+                <input type="number" value={cambio} onChange={handleCambioChange} />
+            </div>
         </div>
     );
 };
